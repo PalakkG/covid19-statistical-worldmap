86d11780306743a95fbd9aa8f15d9e17392e9a96
PalakkG <palak.pkvp@gmail.com>
2021-01-19 23:33:24 +0530

    added range feature

--- src/components/CustomData.js
+++ src/components/CustomData.js
@@ -24,6 +24,7 @@ const CustomData = (props) => {
         };
 
         fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     // const triggerUseEffect = () => {
